125edb12f4fb9524f70fe755591e0190885778a5
Biji Telaso <izzyahnaf695@gmail.com>
2024-01-15 08:56:27 +0700

    commit

--- Izzy/frontend/src/elements/ScrollView.jsx
+++ Izzy/frontend/src/elements/ScrollView.jsx
@@ -14,7 +14,7 @@ const ScrollView = ({ data, formattedNumber }) => {
     <div style={scrollViewContainerStyle}>
       {data.map((item, index) => (
         <div className="bg-gray-400 p-2 rounded-xl items-center" key={index} style={{ display: 'inline-block', marginRight: '10px', width: '180px', height: '95%' }}>
-          <img src="../No-image-found.jpg" className="my-auto border border-gray-500 rounded-lg m-2 shadow-md mb-2 mt-1" style={{ width: '150px', height: '150px' }} alt={`item-${index}`} />
+          <img src="No-image-found.jpg" className="my-auto border border-gray-500 rounded-lg m-2 shadow-md mb-2 mt-1" style={{ width: '150px', height: '150px' }} alt={`item-${index}`} />
           <p className="m-2 text-white font-semibold">{item.name}</p>
           <p className="m-2 text-white text-end">{formattedNumber(item.price)}</p>
         </div>
