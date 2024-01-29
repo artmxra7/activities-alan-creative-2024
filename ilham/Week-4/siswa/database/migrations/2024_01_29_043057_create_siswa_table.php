80a14b9933c0bd164d0d418f25997f6158d207bc
ilham <syaifulilham1407@gmail.com>
2024-01-29 17:09:39 +0700

    new commit

---
+++ ilham/Week-4/siswa/database/migrations/2024_01_29_043057_create_siswa_table.php
@@ -0,0 +1,24 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswaTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('nik');
+            $table->string('alamat');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
